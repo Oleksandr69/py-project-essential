05e7785608b53c43fad037724042c2c62602baa0
Oleksandr Shevchenko <shevchol69@gmail.com>
2025-10-16 21:59:46 +0300

    bullet added

--- main.py
+++ main.py
@@ -3,16 +3,54 @@ import pygame
 import os
 import random
 from modules.classes import *
+from modules.mapsettings import map_1
+
 
 pygame.init()
 
+x = 0
+y = 0
+blocks_list = []
+
+background_1 = pygame.image.load(os.path.join(PATH, 'images/background_1.png'))
+background_1 = pygame.transform.scale(background_1, (SCREEN_WIDTH, SCREEN_HIGHT))
+block_img1 = os.path.join(PATH, 'images/wall_6.png')
+block_img2 = os.path.join(PATH, 'images/wall_1.png')
+block_img3 = os.path.join(PATH, 'images/city_1.png')
+
+for row in map_1:
+    for i in row:
+        if i == 1:
+            blocks_list.append(Block(x, y, 1, block_img1))
+        elif i == 2:
+            blocks_list.append(Block(x, y, 2, block_img2))
+        elif i == 3:
+            blocks_list.append(Block(x, y, 3, block_img3))
+        x += STEP
+    y += STEP
+    x = 0
+# image_1 = pygame.image.load(os.path.join(PATH, 'images/tank_x.png'))
+player_1 = Player1(1, 1)
+# image_2 = pygame.image.load(os.path.join(PATH, 'images/tank_y.png'))
+player_2 = Player2(22, 11)
+
+
+clock = pygame.time.Clock()
 is_running = True
 
 while is_running:
-    window.fill((200, 0, 0))
+    window.fill((100, 150, 200))
+    # window.blit(background_1, (0, 0))
+    for item in blocks_list:
+        item.blit()
+    player_1.bullet.move()
+    player_2.bullet.move()
+    player_1.blit()
+    player_2.blit()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+    clock.tick(10)
     pygame.display.flip()
 
 
--- modules/classes.py
+++ modules/classes.py
@@ -1,9 +1,10 @@
 import pygame
 import os
+from modules.mapsettings import map_1
 
 PATH = os.path.abspath(__file__+ '/../..')
 SCREEN_WIDTH = 1200
-SCREEN_HIGHT = 600
+SCREEN_HIGHT = 650
 STEP = 50
 
 window = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HIGHT))
@@ -15,17 +16,117 @@ class Block(pygame.Rect):
         self.image = pygame.image.load(img)
         self.image = pygame.transform.scale(self.image, (STEP, STEP))
         self.type_wall = type_wall
+        
     def blit(self):
         window.blit(self.image, (self.x, self.y))
 
 class Bullet(pygame.Rect):
-    pass
+    def __init__(self, x, y):
+        super().__init__(x, y, 20, 20)
+        self.image = pygame.image.load(os.path.join(PATH, 'images/bullet_1.png'))
+        self.image = pygame.transform.scale(self.image, (15, 15))
+        self.direction = None
+        self.speed = 50
+        self.count = 0
+    def move(self):
+        if self.count != 0:
+            window.blit(self.image, (self.x, self.y))
+            if self.direction == 0:
+                self.y -= self.speed
+            elif self.direction == 180:
+                self.y += self.speed
+            elif self.direction == 90:
+                self.x -= self.speed
+            elif self.direction == 270:
+                self.x += self.speed
+            self.count -= 1
+            if self.count == 0:
+                self.stop
+    def stop(self):
+        self.count = 0
+        self.x = 0
+
 class Panzar(pygame.Rect):
-    pass
+    def __init__(self, x, y):
+        super().__init__(x * STEP, y * STEP, STEP, STEP)
+        self.image = None
+        self.position = [x, y]
+        self.bullet = Bullet(x, y)
+        self.angle = 0  
+    def move(serlf):
+        pass
+    def blit(self):
+        self.move()
+        window.blit(self.image, (self.x, self.y))
+    def rotate_to(self, angle):
+        rotate = (360 - self.angle + angle)
+        self.angle = angle
+        self.image = pygame.transform.rotate(self.image, rotate)
+    def bullet_from_panzar(self):
+        if self.bullet.count == 0:
+            self.bullet.x = self.x + STEP / 2 - 10
+            self.bullet.y = self.y + STEP / 2 - 10
+            self.bullet.count = 10
+            self.bullet.direction = self.angle
+
 class Player1(Panzar):
-    pass
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.image.load(os.path.join(PATH, 'images/tank_x.png'))
+        self.image = pygame.transform.scale(self.image, (STEP, STEP))
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_w]:
+            if map_1[self.position[1] - 1][self.position[0]] == 0:              
+                self.y -= STEP
+                self.position[1] -= 1
+            self.rotate_to(0)
+        elif keys[pygame.K_s]:
+            if map_1[self.position[1] + 1][self.position[0]] == 0:
+                self.y += STEP
+                self.position[1] += 1
+            self.rotate_to(180)
+        elif keys[pygame.K_a]:
+            if map_1[self.position[1]][self.position[0] - 1] == 0:
+                self.x -= STEP
+                self.position[0] -= 1
+            self.rotate_to(90)
+        elif keys[pygame.K_d]:
+            if map_1[self.position[1]][self.position[0] + 1] == 0:
+                self.x += STEP
+                self.position[0] += 1
+            self.rotate_to(270)
+        elif keys[pygame.K_c]:
+            self.bullet_from_panzar()
 class Player2(Panzar):
-    pass
+    def __init__(self, x, y):
+        super().__init__(x, y)
+        self.image = pygame.image.load(os.path.join(PATH, 'images/tank_y.png'))
+        self.image = pygame.transform.scale(self.image, (STEP, STEP))
+    def move(self):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_UP]:
+            if map_1[self.position[1] - 1][self.position[0]] == 0:              
+                self.y -= STEP
+                self.position[1] -= 1
+            self.rotate_to(0)
+        elif keys[pygame.K_DOWN]:
+            if map_1[self.position[1] + 1][self.position[0]] == 0:
+                self.y += STEP
+                self.position[1] += 1
+            self.rotate_to(180)
+        elif keys[pygame.K_LEFT]:
+            if map_1[self.position[1]][self.position[0] - 1] == 0:
+                self.x -= STEP
+                self.position[0] -= 1
+            self.rotate_to(90)
+        elif keys[pygame.K_RIGHT]:
+            if map_1[self.position[1]][self.position[0] + 1] == 0:
+                self.x += STEP
+                self.position[0] += 1
+            self.rotate_to(270)
+        elif keys[pygame.K_c]:
+            self.bullet_from_panzar()
 
 # class Panzar:
 #     def __init__(self, x, y, width, hight):
